672f65e03e1b13fdc78ba9b6fa803a1f09d96d1a
CypherDon <58037452+yourgodfather12@users.noreply.github.com>
2024-02-24 02:48:22 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+import discord
+from discord.ext import commands
+from commands import bot as custom_bot  # Import the bot instance from commands.py
+
+# Bot token
+TOKEN = 'MTIwOTczMDY4Mjk1NjU1NDM0Mg.GFHpPP.etbdsOFlbD0ahdPcitrWrTyexhUT_DpCuGodkM'
+
+# Run the bot
+custom_bot.run(TOKEN)
